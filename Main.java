b52c8213d457fb19ec7071434f6cee76ebc8d7b3
Abhishek Kumar <166202255+abhishek-kr03@users.noreply.github.com>
2024-04-06 20:01:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+package com.arrayprograms;
+
+import java.util.Scanner;
+
+// Minimum/Smallest pair sum among the given array elements (Positive or Negative)
+
+public class Main {
+	static int secondSmallestElement(int[] arr){
+		int smallest = Integer.MAX_VALUE;
+		int secondSmallest = Integer.MAX_VALUE;
+		for (int i=0 ; i<arr.length ; i++) {
+			if(arr[i] < smallest) {
+				secondSmallest = smallest;
+				smallest = arr[i];
+			}
+			else if(arr[i] < secondSmallest && arr[i] != smallest) {
+				secondSmallest = arr[i];
+			}
+		}
+		return secondSmallest + smallest;
+	}	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the length of the Array:");
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the Elements of the Array:");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		int res = secondSmallestElement(arr);
+		System.out.println("The minimum sum of pairs among the given element is:");
+		System.out.println(res);
+	}
+}
