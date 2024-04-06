3eda87be181b74ded7d05e7927630eb4407469ca
Abhishek Kumar <166202255+Abhishek-Kr03@users.noreply.github.com>
2024-04-06 14:45:09 +0530

    Java Program
    Second smallest element in an array

---
+++ SecondSmallestElementInAnArray.java
@@ -0,0 +1,36 @@
+package com.arrayprograms;
+
+import java.util.Scanner;
+
+// Second Maximum/Largest element inside an array
+
+public class SecondSmallestElementInAnArray {
+	static int secondLargestElement(int[] arr){
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+		for (int i=0 ; i<arr.length ; i++) {
+			if(arr[i] > largest) {
+				secondLargest = largest;
+				largest = arr[i];
+			}
+			else if(arr[i] > secondLargest && arr[i] != largest) {
+				secondLargest = arr[i];
+			}
+		}
+		return secondLargest;
+	}	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the length of the Array:");
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the Elements of the Array:");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		int res = secondLargestElement(arr);
+		System.out.println("The second largest element in the array is:");
+		System.out.println(res);
+	}
+}
